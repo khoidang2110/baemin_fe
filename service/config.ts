6ed53e07e30e077a33bafb10ce7910617f1d0dff
“khoidang2110” <khoidang2110@gmail.com>
2024-09-21 13:00:55 +0700

    up vps

--- service/config.ts
+++ service/config.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 console.log("process.env",process.env)
 export const axiosClient = axios.create({
-     baseURL: 'http://localhost:8080',
-   // baseURL:'http://14.225.218.217:8081',
+    //  baseURL: 'http://localhost:8080',
+   baseURL:"https://api.easybadwork.com/baemin/",
 
 
   });
